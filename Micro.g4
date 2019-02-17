9e02ed82aeec4c87163d56fb69702f64936087d6
osama-abuhamdan <osamaabuhamdan@yahoo.com>
2019-02-18 00:26:27 +0200

    Initial commit

---
+++ Micro.g4
@@ -0,0 +1,7 @@
+// Define a grammar called Micro
+grammar Micro;
+
+
+
+WS : (' '|'\t'|'\r'|'\n')+ -> skip ;  // skip spaces, tabs, newlines
+
---
+++ src/main/java/Driver.java
@@ -0,0 +1,31 @@
+
+// import antlr
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Driver {
+
+	public static void main(String[] args) throws Exception{
+		// read input MICRO code
+		InputStream is=null;
+		try{
+			String inputFile;
+			inputFile = args[0];
+			is = new FileInputStream(inputFile);
+		}
+		catch ( Exception e){
+			System.out.println("You must specify an input file");
+			System.exit(0);
+		}
+
+		ANTLRInputStream input = new ANTLRInputStream(is);
+		MicroLexer lexer = new MicroLexer(input);
+
+		// add code here to print each token’s type and value
+
+	}
+
+}
