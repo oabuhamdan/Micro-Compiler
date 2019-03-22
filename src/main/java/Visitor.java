e11cf42ea8a6fdcadb73ea2500ef3fca05adf03f
osama-abuhamdan <osamaabuhamdan@yahoo.com>
2019-03-22 14:03:04 +0200

    Step3

--- src/main/java/Visitor.java
+++ src/main/java/Visitor.java
@@ -37,6 +37,49 @@ public class Visitor extends MicroBaseVisitor {
      * @param ctx
      */
     @Override
+    public Object visitFuncDecl(MicroParser.FuncDeclContext ctx) {
+        visitFunc_decl(ctx.func_decl());
+        visit(ctx.func_declarations());
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public Object visitNoFuncDecl(MicroParser.NoFuncDeclContext ctx) {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public Object visitFunc_decl(MicroParser.Func_declContext ctx) {
+        visit(ctx.param_decl_list());
+        visitFunc_body(ctx.func_body());
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     *
+     * @param ctx
+     */
+    @Override
     public Object visitStringDecl(MicroParser.StringDeclContext ctx) {
         visitString_decl(ctx.string_decl());
         visit(ctx.decl());
@@ -84,7 +127,50 @@ public class Visitor extends MicroBaseVisitor {
     @Override
     public Object visitVar_decl(MicroParser.Var_declContext ctx) {
         visit(ctx.var_type());
-        visit(ctx.id_list());
+        visitId_list(ctx.id_list());
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public Object visitId_list(MicroParser.Id_listContext ctx) {
+        visitId(ctx.id());
+        visit(ctx.id_tail());
+        return  null;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public Object visitIDTail(MicroParser.IDTailContext ctx) {
+        visitId(ctx.id());
+        visit(ctx.id_tail());
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     *
+     * @param ctx
+     */
+    @Override
+    public Object visitNoTail(MicroParser.NoTailContext ctx) {
         return null;
     }
 
